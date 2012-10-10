8d4ab70e2d1736fe630ab3a3f2168150290c8357
James Shore <jshore@jamesshore.com>
2012-10-10 14:36:08 -0700

    Installed Mocha

---
+++ src/_example_test.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
+(function() {
+	"use strict";
+	
+	describe("Example", function () {
+		it("should run", function () {
+			expect(code.foo()).to.equal("foo");
+		});
+	});
+}());
